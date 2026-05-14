885ba06a2571510af8e172c714316ce9df450814
turtton <top.gear7509@turtton.net>
2026-05-14 21:36:25 +0900

    🔧 Add flake file

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+  inputs.treefmt-nix = {
+    url = "github:numtide/treefmt-nix";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      treefmt-nix,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = treefmt-nix.lib.mkWrapper pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixfmt.enable = true;
+            yamlfmt.enable = true;
+          };
+        };
+        devShells.default = pkgs.mkShell { packages = [ pkgs.bashInteractive ]; };
+      }
+    );
+}
